040e9c0997bf08f65e65135d8f3d17883ef85428
davidkhan <daoud@clarybusinessmachines.com>
2020-09-26 02:29:00 +0500

    added ws url

--- src/apolloProvider.js
+++ src/apolloProvider.js
@@ -23,7 +23,7 @@ const authLink = setContext((_, { headers }) => {
 httpLink = authLink.concat(httpLink) 
 const host = window.location.host
 const wsLink = new WebSocketLink({
-  uri: `ws://${host}/graphql/`,
+  uri: `wss://jessillaine.herokuapp.com/graphql/`,
   options: {
     reconnect: true,
     connectionParams: {
