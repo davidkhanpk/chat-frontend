7b0eda58db8340d3f08128b45f670ac521b47f10
davidkhan <daoud@clarybusinessmachines.com>
2020-09-27 00:30:51 +0500

    change wss url

--- src/apolloProvider.js
+++ src/apolloProvider.js
@@ -1,55 +1,60 @@
-import React from 'react'
-import { ApolloClient, InMemoryCache, ApolloProvider as Provider, createHttpLink, split } from '@apollo/client';
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider as Provider,
+  createHttpLink,
+  split,
+} from "@apollo/client";
 
-import { setContext } from '@apollo/client/link/context';
-import { WebSocketLink } from '@apollo/client/link/ws'
-import { getMainDefinition } from '@apollo/client/utilities'
+import { setContext } from "@apollo/client/link/context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 let httpLink = createHttpLink({
-  uri: 'https://jessillaine.herokuapp.com/',
+  uri: "https://jessillaine.herokuapp.com/",
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : "",
-    }
-  }
+    },
+  };
 });
 
-httpLink = authLink.concat(httpLink) 
-const host = window.location.host
+httpLink = authLink.concat(httpLink);
+const host = window.location.host;
 const wsLink = new WebSocketLink({
-  uri: `wss://jessillaine.herokuapp.com/graphql/`,
+  uri: `wss://jessillaine.herokuapp.com/`,
   options: {
     reconnect: true,
     connectionParams: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`
-    }
-  }
-})
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  },
+});
 
 const splitLink = split(
-  ({query}) => {
+  ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === 'OperationDefinition' && definition.operation === "subscription"
-    )
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
   },
   wsLink,
   httpLink
-)
-
-
+);
 
 const client = new ApolloClient({
   link: splitLink,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 export default function ApolloProvider(props) {
-    return <Provider client={client} {...props} />
+  return <Provider client={client} {...props} />;
 }
